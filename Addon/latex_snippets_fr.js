301984ba28b0ba3dc194fe91043a247547b0f3b1
LightEZu <uwulightezu75019@gmail.com>
2026-09-10 13:43:26 +0200

    Sauvegarde Auto : 10-09-2026 13:43:26

--- Addon/latex_snippets_fr.js
+++ Addon/latex_snippets_fr.js
@@ -380,7 +380,4 @@ export default [
 		priority: 2,
 		description: "Bloc mathématique dans une liste"
 	},
-
-	// Snippet de test : vérifie si Obsidian/LaTeX Suite reçoit bien Shift-Tab
-	{trigger: "", replacement: "", options: "tA", triggerKey: "Shift-Tab", description: "Test détection Shift-Tab"},
 ]
